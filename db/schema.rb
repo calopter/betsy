d34315fa42901768f5176db5fb0f1c71828d030f
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-23 14:42:29 -0700

    order_item model and migrations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_214011) do
+ActiveRecord::Schema.define(version: 2019_10_23_214112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.string "shipping_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "status"
