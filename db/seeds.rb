1bb2948a38cb25f5649c6b24152ab186f905de18
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-10-23 21:24:53 -0700

    seed file template

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+
+REVIEW_FILE = Rails.root.join('db', 'seed_data'),'reviews.csv'
+puts "Loading raw review data from #{REVIEW_FILE}"
+
+review_failures = []
+
+CSV.foreach(REVIEW_FILE, :headers => true) do |row|
+  review = Review.new
+  review.id = row['id']
+  review.product_id = row['product_id']
+  review.message = row['message']
+  review.rating = row['rating']
+  successful = review.save
+  
+  if !successful
+    review_failures << review
+    puts "Failed to save review: #{review.inspect}"
+  else
+    puts "Created review: #{review.inspect}"
+  end
+end
+
+puts "Added #{Review.count} review records"
+puts "#{review_failures.length} reviews failed to save "
