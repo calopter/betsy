de00831513a4d9b51e895b879943a42fcafcdc0b
Mira <calopter@gmail.com>
2019-10-24 18:58:36 -0700

    OrderItem#total

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 describe OrderItem do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'relations' do
+    it 'has a product' do
+      expect(OrderItem.new).must_respond_to :product
+    end
+    
+    it 'has an order' do
+      expect(OrderItem.new).must_respond_to :order
+    end
+  end
+  
+  describe 'custom methods' do
+    describe 'total' do
+      it 'returns an integer total price in cents' do
+        prod = Product.create(price: 2)
+        user = User.create
+        order = Order.create(user_id: user.id)
+        order_item = OrderItem.create(quantity: 2, product_id: prod.id, order_id: order.id)
+
+        total = order_item.total
+        expect(total).must_be_instance_of Integer
+        expect(total).must_equal 4
+      end
+    end
+  end
 end
