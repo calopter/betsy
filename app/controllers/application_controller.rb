1bbf98c12fb4a2a271f55177b7fdb11345cd8769
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-24 22:53:26 -0700

    render and finduser edited to get the logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    protect_from_forgery with: :exception
+
+    before_action :find_user
+
+
+   
+  
+    def render_404
+      # DPR: this will actually render a 404 page in production
+      raise ActionController::RoutingError.new("Not Found")
+    end
+  
+    private
+  
+    def find_user
+      if session[:user_id]
+        @login_user = User.find_by(id: session[:user_id])
+      end
+    end
 end
