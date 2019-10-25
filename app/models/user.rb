701199c163e344ee921f8b029f7005fe56a6656a
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-24 22:54:59 -0700

    self method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ApplicationRecord
   has_many :products
   has_many :orders
   has_many :reviews
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.username = auth_hash["info"]["email"]
+    user.email = auth_hash["info"]["email"]
+
+
+    # Note that the user has not been saved.
+    # We'll choose to do the saving outside of this method
+    return user
+  end
 end
