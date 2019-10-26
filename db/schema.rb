7326cf85fb321cc3a2fecdbeb448b98165242012
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-26 12:13:54 -0700

    same name migration was deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_212917) do
+ActiveRecord::Schema.define(version: 2019_10_25_013441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,15 +83,12 @@ ActiveRecord::Schema.define(version: 2019_10_24_212917) do
     t.string "billing_zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.integer "uid"
-    t.string "provider"
-=======
     t.string "street_address"
     t.string "city"
     t.string "state"
     t.string "mailing_zip"
->>>>>>> master
+    t.integer "uid"
+    t.string "provider"
   end
 
   add_foreign_key "order_items", "orders"
