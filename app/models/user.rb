e3be6072af33a8f5d94f51dcc1bce0f193f6a51c
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-27 20:38:34 -0700

    minor fixes to user model and products.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,6 @@ class User < ApplicationRecord
     user.provider = "github"
     user.username = auth_hash["info"]["nickname"]
     user.email = auth_hash["info"]["email"]
-
-
-    # Note that the user has not been saved.
-    # We'll choose to do the saving outside of this method
     return user
   end
 end
