e9581a3d4dca6abda507d37c548f6eb2d5edb327
Mira <calopter@gmail.com>
2019-10-28 16:30:46 -0700

    tests for updating cart quantities

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,11 +1,33 @@
 class OrderItemsController < ApplicationController
+  def update
+    order_item = OrderItem.find_by(id: params[:id])
+    
+    if find_cart.order_items.include?(order_item)
+      order_item.quantity = order_item_params[:quantity]
+      order_item.save
+      flash[:success] = "updated #{ order_item.product.name } quantity"
+      redirect_to cart_path
+    else
+      flash[:error] = "unable to update quantity"
+      redirect_to cart_path
+    end
+  end
+  
   def destroy
     order_item = OrderItem.find_by(id: params[:id])
     if order_item && find_cart.order_items.include?(order_item)
       order_item.delete
+      flash[:success] = "removed #{ order_item.product.name }"
       redirect_to cart_path
     else
+      flash[:error] = "unable to remove #{ order_item.product.name }"
+      flash[:messages] = ["not found"]
       return head :not_found
     end
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:quantity)
   end
 end
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -2,12 +2,39 @@ require "test_helper"
 
 describe OrderItemsController do
   describe 'update' do
-    
+    it 'changes quantity on existing order_item in cart, and sets flash' do
+      add_to_cart
+      params = { order_item: { quantity: 2 } }
+
+      expect{ patch update_cart_path(find_cart.order_items.first.id), params: params }.wont_change "OrderItem.count"
+      
+      expect(@cart.order_items.first.quantity).must_equal 2
+      
+      must_redirect_to cart_path
+    end
+
+    it 'sets flash and redirects for a non-existing order_item' do
+      add_to_cart
+      params = { order_item: { quantity: 2 } }
+      
+      expect{ patch update_cart_path(-1), params: params }.wont_change "OrderItem.count"
+      assert flash[:error]
+      
+      must_redirect_to cart_path    end
+
+    it 'sets flash and redirects for an existing order_item not in cart' do
+      add_to_cart
+      params = { order_item: { quantity: 2 } }
+      
+      expect{ patch update_cart_path(order_items(:o_i_1).id), params: params }.wont_change "OrderItem.count"
+      assert flash[:error]
+      
+      must_redirect_to cart_path
+    end
   end
 
   describe 'destroy' do
     it 'removes existing order_item from the cart and db, then redirects' do
-      
       add_to_cart
       expect(find_cart.order_items.count).must_equal 1
 
