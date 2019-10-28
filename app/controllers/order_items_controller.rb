df8a2a4a35563fb3f5c6e1d70376a0e813a9ccf4
Mira <calopter@gmail.com>
2019-10-28 15:58:59 -0700

    tests for removing from cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,11 @@
 class OrderItemsController < ApplicationController
   def destroy
-    OrderItem.find_by(id: params[:id]).delete
-    redirect_to cart_path
+    order_item = OrderItem.find_by(id: params[:id])
+    if order_item && find_cart.order_items.include?(order_item)
+      order_item.delete
+      redirect_to cart_path
+    else
+      return head :not_found
+    end
   end
 end
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -7,7 +7,6 @@ describe OrderItemsController do
 
   describe 'destroy' do
     it 'removes existing order_item from the cart and db, then redirects' do
-      skip
       
       add_to_cart
       expect(find_cart.order_items.count).must_equal 1
@@ -18,21 +17,17 @@ describe OrderItemsController do
     end
 
     it 'responds with bad request for non-existing order item' do
-      skip
-      
       add_to_cart
 
       expect{ delete remove_from_cart_path(order_items(:o_i_1)) }.wont_change "@cart.order_items.count"
       
       delete remove_from_cart_path(order_items(:o_i_1))
-      must_respond_with :badrequest
+      must_respond_with :not_found
     end
     
     it 'responds with bad request for non-existing cart' do
-      skip
-      
       delete remove_from_cart_path(order_items(:o_i_1))
-      must_respond_with :badrequest
+      must_respond_with :not_found
     end
   end
 end
