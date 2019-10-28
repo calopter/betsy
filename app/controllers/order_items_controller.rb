d3172c33067576da95cd7490cc4c7f3eb545e938
Mira <calopter@gmail.com>
2019-10-28 15:37:52 -0700

    remove items from cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,6 @@
 class OrderItemsController < ApplicationController
   def destroy
-    get_cart.order_items.find{ |oi| oi.id == params[:id] }.delete
+    OrderItem.find_by(id: params[:id]).delete
     redirect_to cart_path
   end
 end
