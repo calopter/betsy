b3dbf215e3f246901c0bee1c8bec82986c02ff0c
Mira <calopter@gmail.com>
2019-10-29 09:43:16 -0700

    fixes misplaced private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,24 +4,7 @@ class UsersController < ApplicationController
   end
   
   def login
-    username = params[:user][:username]
-    @user = User.find_by(username: username)
     
-    if @user 
-      session[:user_id] = @user.id
-      flash[:message] = "Successfully logged in as #{@user.username}"
-      redirect_to root_path
-    else
-      @user = User.new(username: params[:user][:username])
-      session[:user_id] = @user.id
-      if @user.save
-        flash[:message] = "successfully logged in as new user #{@user.username}"
-        redirect_to root_path
-      else 
-        flash[:message] = "Username can't be empty!"
-        redirect_to root_path
-      end
-    end
   end
   
   def current
@@ -38,12 +21,6 @@ class UsersController < ApplicationController
     flash[:message] = "You have logged out successfully."
     redirect_to root_path
   end
-  
-  private
-  def user_params
-    params.require(:user).permit(:username)
-  end
-  
   
   def find_user
     @current_user = User.find_by(id: session[:user_id])
