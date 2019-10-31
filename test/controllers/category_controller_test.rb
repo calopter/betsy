5d81628de497792fdd59485528ce5ca94a6d8d60
Sara Shah Baig <sarashahbaig@gmail.com>
2019-10-30 20:22:02 -0700

    test added

--- test/controllers/category_controller_test.rb
+++ test/controllers/category_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 describe CategoryController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
-end
+    describe "index action" do
+      
+      it "gives back a successful response" do
+        get category_path
+        must_respond_with :success
+      end
+      
+      it "returns with a success if there are no categories available" do
+        get category_path
+        must_respond_with :success
+      end
+    end
+  end
